2a96539b8505e77228887fdc9fc22d368c358985
philLITERALLY <wpmckinney@gmail.com>
2021-03-05 15:51:37 +0000

    Mock AIO

---
+++ mock_aio.py
@@ -0,0 +1,8 @@
+
+
+def RelInPortB(port, other):
+    print('RelInPortB ', port, ' - ', other)
+    return 0
+    
+def RelOutPort(port, other, other2):
+    print('RelOutPort ', port, ' - ', other, ' - ', other2)
