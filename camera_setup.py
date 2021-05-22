e0c8d3a9e8819d85dcc3e0f188db3eee3a22696c
philLITERALLY <wpmckinney@gmail.com>
2021-05-22 16:50:12 +0100

    Speed up?

--- camera_setup.py
+++ camera_setup.py
@@ -14,8 +14,10 @@ import handle_config
 # bufferless VideoCapture
 class VideoCapture:
   def __init__(self, camera):
+    self.needImg = False
     self.camera = camera
     self.cap = cv2.VideoCapture(camera, cv2.CAP_DSHOW)
+    
     self.cap.set(3, handle_config.CAM_WIDTH)                # CAM WIDTH
     self.cap.set(4, handle_config.CAM_HEIGHT)               # CAM HEIGHT
     # self.cap.set(5, handle_config.CAM_FPS)
@@ -35,10 +37,6 @@ class VideoCapture:
   # read frames as soon as they are available, keeping only most recent one
   def _reader(self):
     while True:
-      _, _ = self.cap.read()
-
-  def read(self):
-    while True:
       ret, frame = self.cap.read()
       if not ret:                   # capture frame error
         print('Image read error')
@@ -46,7 +44,16 @@ class VideoCapture:
       if np.sum(frame) == 0:        # frame empty for some reason
         continue
 
-      return frame
+      self.frame = frame
+      self.needImg = False
+
+  def read(self):
+    self.needImg = True 
+
+    while self.needImg:
+      continue
+
+    return self.frame
 
   def release(self):
       self.cap.release()
